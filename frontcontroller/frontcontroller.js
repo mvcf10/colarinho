838d32ae30370fdf3f944267067fe19c44906356
Vinicius Figueiredo <mvcf10@gmail.com>
2017-05-24 19:04:45 -0300

    close #5 Ajuste no FrontController conforme solicitado pelo professor

--- frontcontroller/frontcontroller.js
+++ frontcontroller/frontcontroller.js
@@ -4,12 +4,10 @@ var ApplicationController = require('../applicationcontroller/applicationcontrol
 var FrontController = function () {
 	//Construtor
 	this.controller = 'FRONT CONTROLLER';
-	
 }
 
 FrontController.prototype.getController = function () {
 	return this.controller;
-
 }
 
 FrontController.prototype.imprime = function () {
@@ -26,9 +24,25 @@ FrontController.prototype.executaNewsList = function(req,res) {
 	ac.mostraViewNewsList(req,res);
 }
 
-
 FrontController.prototype.executaNewsCreate = function(req,res) {
 	ac = new ApplicationController();
 	ac.mostraViewNewsCreate(req,res);
+}
+
+FrontController.prototype.processaGet = function (req,res) {
+
+    switch(req.url) {
+    case '/':
+        FrontController.prototype.executaNewsList(req,res);
+        break;
+    case '/news':
+        FrontController.prototype.executaNewsList(req,res);
+        break;
+    case '/news/create':
+        FrontController.prototype.executaNewsCreate(req,res);
+        break;    
+    default:
+        FrontController.prototype.executaGenerico(req,res);
+    }
 }
 module.exports = FrontController;
--- frontcontroller/manipulador.js
+++ frontcontroller/manipulador.js
@@ -2,24 +2,9 @@ var Frontcontroller = require('./frontcontroller.js');
 
 const manipulador = (request, response) => {  
   fc = new Frontcontroller();
-
+  
   if (request.method == 'GET') {
-
-    switch(request.url) {
-    case '/':
-        fc.executaNewsList(request,response);
-        break;
-    case '/news':
-        fc.executaNewsList(request,response);
-        break;
-    case '/news/create':
-        fc.executaNewsCreate(request,response);
-        break;    
-    default:
-        fc.executaGenerico(request,response);
-    } 
-
-    
+    fc.processaGet(request, response); 
   }
 
   else if (request.method == 'POST') {
